ad9502e040c8d7fb33c4d2a950f1116928ac3496
Frédéric Léveillée <frederic.leveillee@firstwordgroup.com>
2019-08-02 01:06:45 -0400

    - Removed auto-refresh, performance was mediocre. - Set canvas width/height and id - Added keydown listener - Finalized context.drawImage call - mainCharacter is now private

--- src/classes/Canvas.ts
+++ src/classes/Canvas.ts
@@ -1,21 +1,27 @@
 import Character from './Character';
 
 class Canvas {
-  public mainCharacter: Character | null = null;
+
+  private _mainCharacter: Character | null = null;
   protected width: number;
   protected height: number;
   protected element: HTMLCanvasElement;
   protected context: CanvasRenderingContext2D | null;
-  protected refreshLoop: number;
 
   public constructor(width: number, height: number) {
     this.width = width;
     this.height = height;
+
     this.element = document.createElement('canvas');
+
+    this.element.width = width;
+    this.element.height = height;
+    this.element.id = 'game-board';
     this.context = this.element.getContext('2d');
     document.body.appendChild(this.element);
-    const self = this;
-    this.refreshLoop = setTimeout((): void => self.refresh(), 1000);
+
+
+    document.addEventListener('keydown', (event): void => this.move(event));
   }
 
   public reset(): void {
@@ -29,15 +35,15 @@ class Canvas {
     if (this.context) {
       if (this.mainCharacter && this.mainCharacter.activeSprite) {
         this.context.drawImage(
-          this.mainCharacter,
+          this.mainCharacter.image,
           this.mainCharacter.activeSprite.xPos,
           this.mainCharacter.activeSprite.yPos,
-          this.mainCharacter.width,
-          this.mainCharacter.height,
+          this.mainCharacter.activeSprite.width,
+          this.mainCharacter.activeSprite.height,
           this.mainCharacter.xPos,
           this.mainCharacter.yPos,
-          this.mainCharacter.width,
-          this.mainCharacter.height,
+          this.mainCharacter.activeSprite.width,
+          this.mainCharacter.activeSprite.height,
         );
       }
     }
@@ -51,21 +57,25 @@ class Canvas {
         case 'w':
         case 'ArrowUp':
           this.mainCharacter.moveUp();
+          this.refresh();
           console.log('Move Up');
           break;
         case 'a':
         case 'ArrowLeft':
           this.mainCharacter.moveLeft();
+          this.refresh();
           console.log('Move Left');
           break;
         case 's':
         case 'ArrowDown':
           this.mainCharacter.moveDown();
+          this.refresh();
           console.log('Move Down');
           break;
         case 'd':
         case 'ArrowRight':
           this.mainCharacter.moveRight();
+          this.refresh();
           console.log('Move Right');
           break;
       }
@@ -75,6 +85,16 @@ class Canvas {
   public refresh(): void {
     this.reset();
     this.draw();
+  }
+
+  public get mainCharacter(): Character | null {
+    return this._mainCharacter;
+  }
+
+  public set mainCharacter(value: Character | null) {
+    console.log('seting up');
+    this._mainCharacter = value;
+    this.refresh();
   }
 }
 
