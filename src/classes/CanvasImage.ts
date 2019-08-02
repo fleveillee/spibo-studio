84a214b04917ce50b4c9ecc5daa72361894e521a
Frédéric Léveillée <frederic.leveillee@firstwordgroup.com>
2019-08-02 00:48:39 -0400

    CanvasImage can't extend Image; it now holds an _image property.

--- src/classes/CanvasImage.ts
+++ src/classes/CanvasImage.ts
@@ -1,13 +1,18 @@
 import Sprite from './Sprite';
 
-abstract class CanvasImage extends Image {
+abstract class CanvasImage {
   public xPos: number = 0;
   public yPos: number = 0;
   public sprites: Sprite[] = [];
+  private readonly _image: HTMLImageElement;
 
   protected constructor(src: string, width?: number, height?: number) {
-    super(width, height);
-    this.src = src;
+    this._image = new Image(width, height);
+    this._image.src = src;
+  }
+
+  public get image(): HTMLImageElement {
+    return this._image;
   }
 
 }
